85252e199d4ef108fb63275d7e0db1b70e4a61da
thieuan <thieuan85@gmail.com>
2018-08-02 12:48:25 +0700

    Update truffle.js

--- eth/SolAnToken/truffle.js
+++ eth/SolAnToken/truffle.js
@@ -1,11 +1,20 @@
+let mnemonic = "";    //For your wallet
+let token = "";     //To use infura
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
-    development: {
+    development: {        // Ganache
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/' + token, 0),
+      network_id: 3,
+      gas: 4700000
     }
+
   }
 };
